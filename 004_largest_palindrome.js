bfa78fc1aef9a991177c25152eef1b7fe1fb6874
qxia <qxqinxia@outlook.com>
2023-08-25 22:57:45 +0200

    note

---
+++ 004_largest_palindrome.js
@@ -0,0 +1,15 @@
+/*1. find the range: [10 ** (n - 1), 10 ** n - 1]
+2. calculate all possible product
+3. check for palindromes
+
+reverse a string in javascript:
+function reverseString(str) {
+	return str.split('').reverse().join('');
+}
+
+a palindrome:
+num.toString() === num.toString().split('').reverser('').join('')
+
+or a palindrome = s + s.toString().split('').reverser('').join('')*/
+
+
